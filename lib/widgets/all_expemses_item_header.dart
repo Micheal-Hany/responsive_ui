3a8046022d915a654b3db935f58d95efd7475b4a
micheal_hany <michealhany991@gmail.com>
2024-03-13 12:28:30 +0200

    update all_expemses_item_header.dart

--- lib/widgets/all_expemses_item_header.dart
+++ lib/widgets/all_expemses_item_header.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_ui/utils/app_images.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-// 4EB7F2
 class AllExpensesItemHeader extends StatelessWidget {
   const AllExpensesItemHeader({
     super.key,
